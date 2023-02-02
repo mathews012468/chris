240c70cea70ca86c38793a193985561f06281e77
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-02 17:20:53 -0500

    make sure to always send the email in the hourly update

--- updates.sh
+++ updates.sh
@@ -2,4 +2,4 @@ BASEDIR=$(dirname "$BASH_SOURCE")
 
 set -o allexport && source $BASEDIR/.env && set +o allexport
 source $BASEDIR/venv/bin/activate
-python $BASEDIR/main.py email=whydontiknowthat@gmail.com
+python $BASEDIR/main.py email=whydontiknowthat@gmail.com should_send=true
