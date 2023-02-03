42ae94f7ae170f2a8c622f3dc06c8eba758ccebf
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-03 12:58:05 -0500

    remove source so easier to run by cron

--- alerts/updates.sh
+++ alerts/updates.sh
@@ -1,5 +1,5 @@
 BASEDIR=$(dirname "$BASH_SOURCE")
 
-set -o allexport && source $BASEDIR/.env && set +o allexport
-source $BASEDIR/venv/bin/activate
+set -o allexport && . $BASEDIR/.env && set +o allexport
+. $BASEDIR/venv/bin/activate
 python $BASEDIR/main.py email=whydontiknowthat@gmail.com should_send=true
