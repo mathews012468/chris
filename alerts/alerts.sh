b5b7d240e91e1269572531c918ab2d0c0e771f0d
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-03 13:26:58 -0500

    use absolute paths in shell scripts

--- alerts/alerts.sh
+++ alerts/alerts.sh
@@ -1,5 +1,2 @@
-BASEDIR=$(dirname "$BASH_SOURCE")
-
-set -o allexport && . $BASEDIR/.env && set +o allexport
-. $BASEDIR/venv/bin/activate
-python $BASEDIR/main.py email=whydontiknowthat@gmail.com,$CHRIS_EMAIL phone=$CHRIS_PHONE
+set -o allexport && . /app/.env && set +o allexport
+python /app/main.py email=whydontiknowthat@gmail.com,$CHRIS_EMAIL phone=$CHRIS_PHONE
--- alerts/updates.sh
+++ alerts/updates.sh
@@ -1,5 +1,2 @@
-BASEDIR=$(dirname "$BASH_SOURCE")
-
-set -o allexport && . $BASEDIR/.env && set +o allexport
-. $BASEDIR/venv/bin/activate
-python $BASEDIR/main.py email=whydontiknowthat@gmail.com should_send=true
+set -o allexport && . /app/.env && set +o allexport
+python /app/main.py email=whydontiknowthat@gmail.com should_send=true
