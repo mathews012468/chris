529efb6331929e0bb7281369c8cca0491bbf462f
mathews012468 <Mathew.Soto78@myhunter.cuny.edu>
2023-02-03 13:45:49 -0500

    change python to absolute path in shell scripts

--- alerts/alerts.sh
+++ alerts/alerts.sh
@@ -1,2 +1,2 @@
 set -o allexport && . /app/.env && set +o allexport
-python /app/main.py email=whydontiknowthat@gmail.com,$CHRIS_EMAIL phone=$CHRIS_PHONE
+/usr/local/bin/python /app/main.py email=whydontiknowthat@gmail.com,$CHRIS_EMAIL phone=$CHRIS_PHONE
--- alerts/updates.sh
+++ alerts/updates.sh
@@ -1,2 +1,2 @@
 set -o allexport && . /app/.env && set +o allexport
-python /app/main.py email=whydontiknowthat@gmail.com should_send=true
+/usr/local/bin/python /app/main.py email=whydontiknowthat@gmail.com should_send=true
